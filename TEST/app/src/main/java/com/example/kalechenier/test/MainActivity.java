92bed569e1e14a3997579a64f75fc8396ae95d04
Kale Chenier <will_chenier@hotmail.com>
2015-02-24 15:48:56 -0500

    First script

---
+++ TEST/app/src/main/java/com/example/kalechenier/test/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.kalechenier.test;
+
+
+        import android.app.Activity;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.view.View.OnClickListener;
+        import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    private Button button;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+
+    public void onCreate(Bundle savedInstanceState) {
+        final Context context = this;
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        button = (Button) findViewById(R.id.buttonUrl);
+        button2 = (Button) findViewById(R.id.buttonUrl2);
+        button3 = (Button) findViewById(R.id.buttonUrl3);
+        button4 = (Button) findViewById(R.id.buttonUrl4);
+
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(context, WebViewActivity.class);
+                startActivity(intent);
+            }
+
+        });
+        button2.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(context, WebViewActivity.class);
+                startActivity(intent);
+            }
+
+        });
+        button3.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(context, WebViewActivity.class);
+                startActivity(intent);
+            }
+
+        });
+        button4.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(context, WebViewActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+    }
+
+}
---
+++ TEST/app/src/main/java/com/example/kalechenier/test/WebViewActivity.java
@@ -0,0 +1,52 @@
+package com.example.kalechenier.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.Button;
+
+public class WebViewActivity extends Activity {
+
+    private WebView webView;
+    private WebView webView2;
+    private WebView webView3;
+    private WebView webView4;
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview);
+
+
+        webView = (WebView) findViewById(R.id.webView1);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://www.google.com");
+
+        webView2 = (WebView) findViewById(R.id.webView1);
+        webView2.getSettings().setJavaScriptEnabled(true);
+        webView2.loadUrl("http://www.reddit.com");
+
+        webView3 = (WebView) findViewById(R.id.webView1);
+        webView3.getSettings().setJavaScriptEnabled(true);
+        webView3.loadUrl("http://www.facebook.com");
+
+        webView4 = (WebView) findViewById(R.id.webView1);
+        webView4.getSettings().setJavaScriptEnabled(true);
+        webView4.loadUrl("http://www.youtube.com");
+
+
+
+
+        //LOAD CUSTOM WEBPAGE
+        //String customHtml = "<html><body><h1>Hello, WebView</h1></body></html>";
+        //webView.loadData(customHtml, "text/html", "UTF-8");
+
+    }
+
+}
+
+
+
+
+
+
